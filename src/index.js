a13864c68e101461d985f7c140ceee7e17604133
Victor Wang <hongjenwang@gmail.com>
2022-02-28 15:42:41 -0800

    Add css and refactor code into less decomposed format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css'
 
 function BookList() {
   return (
-    <section>
+    <section className='booklist'>
       <Book />
       <Book />
       <Book />
@@ -13,39 +14,20 @@ function BookList() {
 
 const Book = () => {
   return (
-    <section>
-      <Image />
-      <Title />
-      <Author />
-      <Description />
-    </section>
+    <article className="book">
+      <img
+        src="https://m.media-amazon.com/images/I/71CFMKWRYEL._AC_UL320_.gif"
+        alt="The Bicycle Wheel Book"
+      />
+      <h1>Title</h1>
+      <p>AUTHOR NAME</p>
+      <p>
+        This is a paragraph of the description of the book.<br></br>
+        Bla... Bla... Bla...
+      </p>
+    </article>
   )
 }
 
-const Image = () => {
-  return (
-  <img
-    src="https://m.media-amazon.com/images/I/71CFMKWRYEL._AC_UL320_.gif"
-    alt="The Bicycle Wheel Book"
-  />
-  )
-}
-
-const Title = () => {
-  return <p>Title</p>
-}
-
-const Author = () => {
-  return <p>AUTHOR NAME</p>
-}
-
-const Description = () => {
-  return (
-    <p>
-      This is a paragraph of the description of the book.<br></br>
-      Bla... Bla... Bla...
-    </p>
-  )
-}
 
 ReactDom.render(<BookList />, document.getElementById('root'))
