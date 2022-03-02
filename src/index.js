64e4b81f14cfc5b8dee87d08fec71dbdb110ffb1
Victor Wang <hongjenwang@gmail.com>
2022-03-02 13:29:55 -0800

    pass in object in props

--- src/index.js
+++ src/index.js
@@ -1,50 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/81HqUdZxbLL._AC_UL320_.jpg',
-  title: 'Zinn & the Art of Mountain Bike Maintenance',
-  author: 'Lennard Zinn',
-}
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71CFMKWRYEL._AC_UL320_.gif',
-  title: 'The Bicycle Wheel',
-  author: 'Jobst Brandt',
-}
+const books =[
+  {
+    id: 1,
+    image: 'https://m.media-amazon.com/images/I/81HqUdZxbLL._AC_UL320_.jpg',
+    title: 'Zinn & the Art of Mountain Bike Maintenance',
+    author: 'Lennard Zinn',
+  },
+  
+  {
+    id: 2,
+    image: 'https://m.media-amazon.com/images/I/71CFMKWRYEL._AC_UL320_.gif',
+    title: 'The Bicycle Wheel',
+    author: 'Jobst Brandt',
+  },
+]
 
 const Booklist = () => {
   return(
-    <div className='booklist'>
-      <Book 
-        image = {firstBook.img}
-        title = {firstBook.title}
-        author = {firstBook.author}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, dolorum recusandae sequi accusamus eveniet quos beatae harum voluptatem consequuntur esse!
-        </p>
-      </Book>
-      <Book 
-        image = {secondBook.img}
-        title = {secondBook.title}
-        author = {secondBook.author}
-      />
-      <Book />
-    </div>
+    <div className='booklist'>{books.map((book) => {
+      return (
+        <Book 
+        // image = {img} //image comes from Book. img comes from books.map() each book.
+        // title = {title}
+        // author = {author} 
+        key={book.id} book={book} //first book is prop, {book} is what is the book being passed in.
+        />
+      )
+    })}</div>
   )
 }
 
-const Book = (props) => { //children prop is special 
+const Book = (props) => { 
+  console.log(props)
+  const {image, title, author} = props.book
   return(
     <div className='book'>
-      <img src={props.image} />
-      <h1>{props.title}</h1>
-      <h3>{props.author}</h3>
-      {props.children}
+      <img src={image} />
+      <h1>{title}</h1>
+      <h3>{author}</h3>
     </div>
-
   )
 }
 
